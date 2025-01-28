3a05476a9b301e389d384e011a3913d0e1928a38
Trevor Gross <tmgross@umich.edu>
2025-01-28 07:25:29 +0000

    WIP

--- crates/libm-test/src/gen/from_file.rs
+++ crates/libm-test/src/gen/from_file.rs
@@ -17,7 +17,6 @@ pub fn get_test_cases<F: FloatExt>(fname: impl AsRef<Path>) -> impl Iterator<Ite
     // Files can be multiple MB so optimize reading.
     let mut reader = BufReader::new(f);
     let mut buf = String::new();
-    let consts = F::consts();
 
     iter::from_fn(move || {
         let s = loop {
@@ -50,4 +49,65 @@ pub fn get_test_cases<F: FloatExt>(fname: impl AsRef<Path>) -> impl Iterator<Ite
 
         Some(ret)
     })
+}
+
+trait Foo {
+    fn foo(input: &str) -> Self;
+}
+
+macro_rules! impl_parse_tuple {
+    ($ty:ty) => {
+        impl Foo for ($ty,) {
+            fn foo(input: &str) -> Self {
+                (parse(input, 0),)
+            }
+        }
+
+        impl Foo for ($ty, $ty) {
+            fn foo(input: &str) -> Self {
+                (parse(input, 0), parse(input, 1))
+            }
+        }
+
+        impl Foo for ($ty, i32) {
+            fn foo(input: &str) -> Self {
+                (parse(input, 0), parse(input, 1))
+            }
+        }
+
+        impl Foo for (i32, $ty) {
+            fn foo(input: &str) -> Self {
+                (parse(input, 0), parse(input, 1))
+            }
+        }
+
+        impl Foo for ($ty, $ty, $ty) {
+            fn foo(input: &str) -> Self {
+                (parse(input, 0), parse(input, 1), parse(input, 2))
+            }
+        }
+    };
+}
+
+impl_parse_tuple!(f32);
+impl_parse_tuple!(f64);
+
+#[cfg(f16_enabled)]
+impl_parse_tuple!(f16);
+#[cfg(f128_enabled)]
+impl_parse_tuple!(f128);
+
+fn parse_float<F: FloatExt>(s: &str) -> F {
+    let consts = F::consts();
+
+    let ret = match s {
+        "+snan" => consts.min_snan,
+        "-snan" => consts.neg_min_snan,
+        "+qnan" => consts.pos_nan,
+        "-qnnn" => consts.neg_nan,
+        "+inf" => F::INFINITY,
+        "-inf" => F::NEG_INFINITY,
+        _ => F::from_hex_str(s),
+    };
+    ret
 }
